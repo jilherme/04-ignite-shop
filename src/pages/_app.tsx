60cbf575cbc359c4d828d4aa273ebc8cc28843a2
jilherme <rosa.guilhermediogo@gmail.com>
2023-04-16 17:01:00 -0300

    initial project configuration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+
+import { globalStyles } from '@/styles/global'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {  
+  return (
+    <div className={roboto.className}>
+      <Component {...pageProps} />
+    </div>
+  )
+}
