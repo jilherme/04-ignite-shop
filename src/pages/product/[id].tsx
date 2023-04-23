8c0593d79fdca6c22102b3a1b485d912c89fd658
jilherme <rosa.guilhermediogo@gmail.com>
2023-04-22 23:01:42 -0300

    add(product-page): fetch product from stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,71 @@
+import { GetStaticProps } from "next"
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
+import { stripe } from "@/lib/stripe"
+import Stripe from "stripe"
+import Image from "next/image"
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({product}: ProductProps) {
     const { query } = useRouter()
 
     return (
-      <div>
-        <h1>Product: {JSON.stringify(query)}</h1>
-      </div>
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt='' />
+        </ImageContainer>
+
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+
+          <button>Comprar agora</button>
+        </ProductDetails>
+        </ProductContainer>
     )
+  }
+
+  // GetStaticProps recebe alguns generics do TS:
+  // O 1o é qual o retorno de dentro dele, as props
+  // O 2o é quais os parâmetros que ele recebe
+  /*  
+  acho que algo relacionado a config strict
+  nao permitiu usar GetStaticProps<any, {id: string}> 
+  */
+  export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const productId = params?.id
+
+    const product = await stripe.products.retrieve(productId as string, {
+      expand: ['default_price'] // por ser um único produto, nao precisa do data.
+    })
+
+    const price = product.default_price as Stripe.Price
+    const formattedPrice = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(price.unit_amount! / 100)
+
+    return {
+      props: {
+        product: {
+          id: product.id,
+          name: product.name,
+          imageUrl: product.images[0],
+          price: formattedPrice,
+          description: product.description
+        }
+      },
+      revalidate: 60 * 60 * 1 // 1 hour
+    }
   }
